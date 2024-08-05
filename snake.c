f3a6776f617325325ccaf9afb37dc69dc9e96fb3
Arthur Pesi <arthur.pesi@gmail.com>
2024-08-05 20:50:33 -0300

    snake and apple now are drawn in map

--- snake.c
+++ snake.c
@@ -29,21 +29,12 @@ void renderFrame(struct Game *frame) {
     for(x;x < frame->mapSize; x++) {
         int y = 0;
         for(y; y/2 < frame->mapSize; y++) {
-            int drawn = 0;
-            for(int i = frame->snake->tail; i <= frame->snake->head;i++) {
-                if(frame->snake->position[i][0] == x && frame->snake->position[i][1] == y/2) {
-                    if(y % 2 == 0) {
-                        printf("[");
-                    } else {
-                        printf("]");
-                    }
-                    drawn = 1;
+            if(frame->map[x][y/2] == 'S') {
+                 if(y % 2 == 0) {
+                    printf("[");
+                } else {
+                    printf("]");
                 }
-            } 
-            if(drawn) {
-
-            } else if(x == frame->apple[0] && y/2 == frame->apple[1]) {
-                printf("6");
             } else {
                 printf("%c",frame->map[x][y/2]);
             }
@@ -56,9 +47,21 @@ void sortApplePosition(struct Game *game) {
     srand(time(NULL)); //nao nascer dentro da cobra
     game->apple[0] = (rand() % (game->mapSize - 3)) + 1;
     game->apple[1] = (rand() % (game->mapSize - 3)) + 1;
+    game->map[game->apple[0]][game->apple[1]] = '6';
 }
 
-void moveSnake(struct Snake *snake) {
+int gameover(struct Game *currFrame) {
+    const struct Snake *snake = currFrame->snake;
+    const int x = snake->position[snake->head][0];
+    const int y = snake->position[snake->head][1];
+    if(currFrame->map[x][y] == '|') {
+        return 1;
+    } //se a cobra encostar em si mesma
+    return 0;
+}
+
+int moveSnake(struct Game *frame) {
+    struct Snake *snake = frame->snake;
     if(GetAsyncKeyState(UP)) {
         snake->direction[0] = -1;
         snake->direction[1] = 0;
@@ -72,6 +75,9 @@ void moveSnake(struct Snake *snake) {
         snake->direction[0] = 0;
         snake->direction[1] = 1;
     }
+    int tailX = snake->position[snake->tail][0];
+    int tailY = snake->position[snake->tail][1];
+    frame->map[tailX][tailY] = ' ';
     int dir[2];
     dir[0] = snake->direction[0] + snake->position[snake->head][0];
     dir[1] = snake->direction[1] + snake->position[snake->head][1];
@@ -79,6 +85,9 @@ void moveSnake(struct Snake *snake) {
     snake->tail = (snake->tail + 1) % snake->positionSize;
     snake->position[snake->head][0] = dir[0];
     snake->position[snake->head][1] = dir[1];
+    int gameOver = gameover(frame);
+    frame->map[dir[0]][dir[1]] = 'S';
+    return gameOver;
 }
 
 struct Game *initializeGame() {
@@ -100,36 +109,33 @@ struct Game *initializeGame() {
     newGame->snake->size = 1;
     newGame->snake->head = 0;
     newGame->snake->tail = 0;
-    newGame->snake->position[0][0] = (newGame->mapSize-1)/2;
-    newGame->snake->position[0][1] = (newGame->mapSize-1)/2;
+    int initialPosition = (newGame->mapSize-1)/2;
+    newGame->snake->position[0][0] = initialPosition;
+    newGame->snake->position[0][1] = initialPosition;
+    newGame->map[initialPosition][initialPosition] = 'S';
     newGame->snake->direction[0] = 0;
     newGame->snake->direction[1] = 1;
     return newGame;
 }
 
-int gameover(struct Game *currFrame) {
-    const struct Snake *snake = currFrame->snake;
-    const int x = snake->position[snake->head][0];
-    const int y = snake->position[snake->head][1];
-    if(currFrame->map[x][y] == '|') {
-        return 1;
-    } //se a cobra encostar em si mesma
-    return 0;
+void delayFrame() {
+    double frameDuration = 0.5f;
+    double timeTaken;
+    clock_t start = clock();
+    do {
+        timeTaken = (double)(clock() - start) / CLOCKS_PER_SEC;
+    } while(frameDuration > timeTaken);
 }
 
 int main() {
     struct Game *game = initializeGame();
-    renderFrame(game);
-    do {
-        double frameDuration = 0.5f;
-        double timeTaken;
-        clock_t start = clock();
-        do {
-            timeTaken = (double)(clock() - start) / CLOCKS_PER_SEC;
-        } while(frameDuration > timeTaken);
-        moveSnake(game->snake);
+    while(1) {
         renderFrame(game);
-    } while(!gameover(game));
+        delayFrame();
+        if(moveSnake(game)) {
+            break;
+        }
+    }
     free(game->snake);
     free(game);
     return 0;
